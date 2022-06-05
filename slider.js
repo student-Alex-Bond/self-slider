3957a9cbb99480eda73c45301fd1aa9af197ba5f
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-06-05 21:35:13 +0600

    some commit

--- slider.js
+++ slider.js
@@ -3,78 +3,102 @@ const buttonRight = document.querySelector('.arrow_right')
 const slides = document.querySelectorAll('.slides__slide')
 const dots = document.querySelectorAll('.slider__dots-item')
 const widthSlide = document.querySelector('.slides__container').clientWidth;
-
-
-let slider = []
-slides.forEach((item ,index) => {
-	slider.push(item.src)
+//const dots_container = document.querySelector('.slider__dots')
+let sliderSrcImg = []
+slides.forEach((item, index) => {
+	sliderSrcImg.push(item.src)
 	slides[index].remove()
 })
+// variables
+let currentIndex = 0
+let arrayTwoImg = []
 
-let step = 0
-let offset = 0
-
-function createAndAddArrayTwoImage() {
-	let img = document.createElement('img')
-	img.src = slider[step]
-	img.classList.add('slides__slide')
-	img.style.left = offset * widthSlide + 'px'
-	document.querySelector('.slides__container').appendChild(img)
-
-	if (step === slider.length - 1) {
-		step = 0
-	} else {
-		step++
-	}
-	offset = 1
+function templateImg(img, position) {
+	return `<img class="slides__slide" src="${img}" style="left:${position}">`
 }
 
-function changeSlide() {
-	buttonRight.disabled = true
+function renderSlides(arrayImg) {
+	let html = arrayImg.map(img => img).join(' ')
+	document.querySelector('.slides__container').insertAdjacentHTML("afterbegin", html)
+}
+
+function fillArrayImage() {
+	if (arrayTwoImg.length === 0) {
+		for (let i = 0; i < sliderSrcImg.length - 1; i++) {
+			let left = i * widthSlide + 'px'
+			arrayTwoImg.push(templateImg(sliderSrcImg[i], left))
+		}
+	}
+	renderSlides(arrayTwoImg)
+}
+
+function changeSlide(direction) {
+
+	if (direction === 'forward') {
+
+
+	}
+	if (direction === 'back') {
+
+	}
+
+	fillArrayImage()
+}
+
+function animateSlide(button, direction) {
+	button.disabled = true
 	let showSlides = document.querySelectorAll('.slides__slide')
 	let start = Date.now();
 	const timeout = 1000
 	let timer = setInterval(function () {
 		let timePassed = Date.now() - start;
 
-
 		if (timePassed >= timeout + 7) {
 			clearInterval(timer);
 			return;
 		}
-		showSlides[0].style.left = Math.ceil(-(timePassed) / (timeout / widthSlide)) + 'px'
-		showSlides[1].style.left = widthSlide - timePassed / (timeout / widthSlide) + 'px'
+		if (direction === 'forward') {
+			showSlides[0].style.left = -(timePassed) / (timeout / widthSlide) + 'px'
+			showSlides[1].style.left = widthSlide - timePassed / (timeout / widthSlide) + 'px'
+		}
+		if (direction === 'back') {
+
+		}
 
 
 	}, 10);
 	setTimeout(function () {
-		showSlides[0].remove()
-		createAndAddArrayTwoImage()
-		buttonRight.disabled = false
-		activeDot(step)
+		showSlides.forEach(item => item.remove())
+		if (direction === 'forward') {
+			changeSlide(direction)
+		}
+		if (direction === 'back') {
+			changeSlide(direction)
+		}
+		button.disabled = false
+
 
 	}, timeout)
 }
 
 function activeDot(step) {
-	if (step === slider.length - 1) {
-		dots[step].classList.remove('slider__dots-circle--active')
-		dots[0].classList.add('slider__dots-circle--active')
-	} else {
-		dots[step].classList.remove('slider__dots-circle--active')
-		dots[step + 1].classList.add('slider__dots-circle--active')
-	}
+	dots.forEach(dot => dot.classList.remove('slider__dots-circle--active'))
 
+	dots[step].classList.add('slider__dots-circle--active')
 
 }
 
-createAndAddArrayTwoImage()
-createAndAddArrayTwoImage()
+fillArrayImage()
+
 
 buttonRight.addEventListener('click', () => {
-	changeSlide()
+	animateSlide(buttonRight, 'forward')
 })
 buttonLeft.addEventListener('click', () => {
-	changeSlide()
+	animateSlide(buttonLeft, 'back')
 })
+
+// dots_container.addEventListener('click', (event) => {
+// 	changeDots(event)
+// })
 
